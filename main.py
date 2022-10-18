6cb6aa42eb28ab03c7c772e808038af4afbac675
Timmo Hage <attmhage@gmail.com>
2022-10-18 20:09:52 +0200

    Convert networth column to floats

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import csv
+import locale
+
+locale.setlocale(locale.LC_ALL, '')
 
 with open("TopRichestInWorld.csv") as file:
     data = csv.reader(file)
     # Print only the NetWorth column
-    print(*[line[1] for line in list(data)[2:-1]])
+    networths = [line[1] for line in list(data)[2:]]
+
+# Extract float value from string NetWorth column
+converted = [locale.atof(val.strip('$')) for val in networths]
